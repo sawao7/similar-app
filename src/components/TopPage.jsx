88cba3241c5d5f53c32f99520409a3bb1bebb283
澤田悠太 <yuta2929ayax@gmail.com>
2022-06-04 21:19:10 +0900

    almost finish

--- src/components/TopPage.jsx
+++ src/components/TopPage.jsx
@@ -24,25 +24,9 @@ export const TopPage = () => {
 			this.setState({ imageData: null });
 		}
 	};
-	const getImage2 = (e) => {
-		const files = e.target.files;
-		if (files.length > 0) {
-			const file = files[0];
-			const reader = new FileReader();
-			reader.onload = (e) => {
-				console.log(e.target.result);
-				setImage2(e.target.result);
-			};
-			reader.readAsDataURL(file);
-		} else {
-			this.setState({ imageData: null });
-		}
-	};
 
 	return (
 		<div>
-			{/* <div className="white">Reaact</div>
-			<div className="bg-red-500">テスト</div> */}
 			<h1>画像を入力してください</h1>
 
 			<div className="flex justify-around">
@@ -50,10 +34,10 @@ export const TopPage = () => {
 					<input accept="image/*" type="file" onChange={getImage} />
 					<img className="h-48" src={image} alt="none" />
 				</div>
-				<div>
-					<input accept="image/*" type="file" onChange={getImage2} />
-					<img className="h-48" src={image2} alt="none" />
-				</div>
+			</div>
+			<div className="flex justify-around">
+				<img className="" src="" alt="none"/>
+				<img className="" src="" alt="none"/>
 			</div>
 			<div>
 				<p className="">{text}</p>
